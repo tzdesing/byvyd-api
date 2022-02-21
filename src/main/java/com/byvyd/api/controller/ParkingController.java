6b8afd7d6430f65161067453cdcb4839afd4cefc
tkraide <thiagokraide2002@gmail.com>
2022-02-21 15:23:36 -0300

    dto, mapper, controller, service

---
+++ src/main/java/com/byvyd/api/controller/ParkingController.java
@@ -0,0 +1,33 @@
+package com.byvyd.api.controller;
+
+import com.byvyd.api.controller.dto.ParkingDTO;
+import com.byvyd.api.controller.mapper.ParkingMapper;
+import com.byvyd.api.model.Parking;
+
+import com.byvyd.api.service.ParkingService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/parking")
+public class ParkingController {
+
+    private final ParkingService parkingService;
+    private final ParkingMapper parkingMapper;
+
+    public ParkingController(ParkingService parkingService, ParkingMapper parkingMapper) {
+        this.parkingService = parkingService;
+        this.parkingMapper = parkingMapper;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ParkingDTO>> findAll(){
+        List<Parking> parkingList = parkingService.findAll();
+        List<ParkingDTO> result = parkingMapper.toParkingDTOList(parkingList);
+        return ResponseEntity.ok(result);
+    }
+}
